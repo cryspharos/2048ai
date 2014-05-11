5647966c7b30757bf1ca722946db87a76bbb9723
crys <crys@kmc.gr.jp>
2014-05-12 00:21:32 +0900

    移動関数，評価関数の実装
    盤面を上下左右に動かした時の盤面を返す関数を実装した(まだモナドうまく使えなかったから，盤面が動かなかった時は空リストを返す)
    盤面の隣接する数字の差を元にした評価関数を実装した

--- main.hs
+++ main.hs
@@ -4,9 +4,10 @@ import System.Environment (getArgs)
 import Network.HTTP
 import Network.Browser (browse, request)
 import Text.JSON.Generic
+type Grid = [[Int]]
 
-data Data2048 = Data2048 {
-  grid :: [[Int]],
+data State2048 = State2048 {
+  grid :: Grid,
   score :: Int,
   points :: Int,
   moved :: Bool,
@@ -16,10 +17,67 @@ data Data2048 = Data2048 {
   zen :: String
 } deriving (Eq, Show, Data, Typeable)
 
-responseToData :: Response String -> Data2048
+{- リストの右端から左端に詰めるように動かす -}
+moveLastToFirst :: (Bool, [Int]) -> (Bool, [Int])
+moveLastToFirst (b, [])       = (b, [])
+moveLastToFirst (b, [x])      = (b, [x])
+moveLastToFirst (b, 0:0:xs)   =
+    let (b', moved) = (moveLastToFirst (b, 0:xs))
+    in (b', moved ++ [0])
+moveLastToFirst (b, 0:y:xs)   =
+    let (b', moved) = (moveLastToFirst (b, y:xs))
+    in (True, moved ++ [0])
+moveLastToFirst (b, x:y:rest)
+  | x == y                    =
+    let (b', moved) = (moveLastToFirst (b, rest))
+    in (True, [x + y] ++ moved ++ [0])
+  | otherwise                 =
+    let (b', moved) = (moveLastToFirst (b, y:rest))
+    in (b', [x] ++ moved)
+
+{- 4x4の入れ子整数リストを取って転置する -}
+transposition :: Grid -> Grid
+transposition = (foldr $ zipWith (:)) [[],[],[],[]]
+
+{- 4x4の入れ子リストを左に動かす -}
+{- TODO:Maybeとかで実装したい -}
+moveLeft :: Grid -> Grid
+moveLeft grid =
+    let (b0, movedRow0) = moveLastToFirst(False, grid !! 0)
+        (b1, movedRow1) = moveLastToFirst(b0, grid !! 1)
+        (b2, movedRow2) = moveLastToFirst(b1, grid !! 2)
+        (b3, movedRow3) = moveLastToFirst(b2, grid !! 3)
+    in
+      if b3 == True
+      then [movedRow0,movedRow1,movedRow2,movedRow3]
+      else []
+
+moveRight :: Grid -> Grid
+moveRight grid =
+    map reverse $ moveLeft $ map reverse grid
+
+moveUp :: Grid -> Grid
+moveUp grid =
+    transposition $ moveLeft $ transposition grid
+
+moveDown :: Grid -> Grid
+moveDown grid =
+    transposition $ moveRight $ transposition grid
+
+{- 盤面の評価値を返す -}
+calcScore :: Grid -> Int
+calcScore grid =
+    let grid' = (map $ (map (\x -> if x == 0 then 1 else x))) grid
+        log2 = (\x -> if x == 1 then 1 else 1 + log2(x `div` 2))
+        logGrid = (map $ (map log2)) grid'
+        logGridT = transposition logGrid
+        diff (x:y:z:w:_) = (abs (x - y)) + (abs (y - z)) + (abs (z - w))
+    in (+) (sum $ (map diff) logGrid) (sum $ (map diff) logGridT)
+
+responseToData :: Response String -> State2048
 responseToData rsp = decodeJSON $ rspBody rsp
 
-gameLoop :: String -> Data2048 -> Int -> IO String
+gameLoop :: String -> State2048 -> Int -> IO String
 gameLoop host result rnd = if over result
   then return $ show $ score result
   else
